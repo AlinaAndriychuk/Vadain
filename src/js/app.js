39dae89b02c82f3198075b31d85b8fce8f40c52f
alina <alina@coderiver.com.ua>
2021-02-03 16:56:55 +0200

    hover rooms

--- src/js/app.js
+++ src/js/app.js
@@ -5,33 +5,44 @@ import sayHello from './lib/sayHello';
 
 class Animation {
 
-  constructor({playButton, stopButton}) {
-    this.animationContainer = document.querySelector('.animation-block');
+  constructor({container, playButton, stopButton, titlesContainer}) {
+    this.animationContainer = container;
     this.width = this.animationContainer.clientWidth;
     this.height = this.animationContainer.clientHeight;
     this.playButton = playButton;
     this.stopButton = stopButton;
     this.stopAnimation = false;
+    this.titlesContainer = titlesContainer;
+    this.titles = this.titlesContainer.querySelectorAll('.js-title');
 
     this.roomsOptions = [
       {x: 730, y: 10, yPath: 10, speed: 0.01, timer: 0},
-      {x: 235, y: 72, yPath: 10, speed: 0.008, timer: 0},
+      {x: 358, y: 229, yPath: 10, speed: 0.008, timer: 0},
       {x: 229, y: 70, yPath: 10, speed: 0.008, timer: 0},
       {x: 308, y: 406, yPath: 5, speed: 0.01, timer: 0},
       {x: 330, y: 226, yPath: 5, speed: 0.01, timer: 0},
       {x: 48, y: 360, yPath: 10, speed: 0.01, timer: 0},
-      {x: 463, y: 43, yPath: 10, speed: 0.012, timer: 0},
+      {x: 542, y: 183, yPath: 10, speed: 0.012, timer: 0},
       {x: 463, y: 40, yPath: 10, speed: 0.012, timer: 0},
-      {x: 318, y: 404, yPath: 8, speed: 0.013, timer: 0},
+      {x: 408, y: 517, yPath: 8, speed: 0.013, timer: 0},
       {x: 330, y: 420, yPath: 8, speed: 0.013, timer: 0},
-      {x: 274, y: 240, yPath: 9, speed: 0.02, timer: 0},
+      {x: 380, y: 363, yPath: 9, speed: 0.02, timer: 0},
       {x: 274, y: 237, yPath: 9, speed: 0.02, timer: 0},
-      {x: 215, y: 393, yPath: -9, speed: 0.015, timer: 0},
+      {x: 289, y: 490, yPath: -9, speed: 0.015, timer: 0},
       {x: 0, y: 390, yPath: -9, speed: 0.015, timer: 0},
-      {x: 551, y: 201, yPath: -6, speed: 0.015, timer: 0},
+      {x: 661, y: 331, yPath: -6, speed: 0.015, timer: 0},
       {x: 554, y: 205, yPath: -6, speed: 0.015, timer: 0},
     ];
 
+    this.titlesOptions = [
+      {y: -50, x: 520, color: '#f8f7ef', background: '#88979c'},
+      {y: -50, x: 120, color: '#bf9c7a', background: '#f2edd3'},
+      {y: -20, x: 90, color: '#fef9e8', background: '#e1cdab'},
+      {y: 0, x: 600, color: '#b9987a', background: '#f3ebe0'},
+      {y: -50, x: 460, color: '#525a58', background: '#eedacc'},
+      {y: -50, x: 100, color: '#b9987a', background: '#f3ebe0'},
+    ]
+    
     this.canvas = new PIXI.Application({
       width: this.width, 
       height: this.height,
@@ -87,41 +98,18 @@ class Animation {
       this.container.addChild(luxuriousHover);
       this.container.addChild(luxurious);
 
-      this.container.children.forEach ( (room, index) => {
-        room.scale.set(0.6);
-        room.position.set(this.roomsOptions[index].x, this.roomsOptions[index].y);
-      });
+      this.coordinateRooms();
 
-      const hovers = [familyHover, luxuriousHover, marketingHover, stairsHover, trainingHover, youtfulHover];
-
-      /* eslint no-param-reassign: ["error", { "props": false }] */
-      // luxuriousHover.anchor.set(0.5, 0.5);
-      // marketingHover.anchor.set(0.5, 0.5);
-      // trainingHover.anchor.set(0.5, 0.5);
-      // familyHover.anchor.set(0.5, 0.5)
-      // youtfulHover.anchor.set(0.5, 0.5);
-      hovers.forEach ( (hover) => {
-        hover.scale.set(0.52);
-        // hover.anchor.set(0.5, 0.5);
-
-        hover.interactive = true;
-        hover.buttonMode = true;
-        hover.on('pointerover', this.mouseOverRoom.bind(this, hover));
-        hover.on('pointerout', this.mouseOffRoom.bind(this, hover));
-      });
+      this.hovers = [familyHover, luxuriousHover, marketingHover, stairsHover, trainingHover, youtfulHover];
+      this.addHoverListaners();
 
       const rect = new PIXI.Graphics(); // Empty right space
-      rect.drawRect(0, 0, 200, 200);
-      rect.x = this.container.width;
+      rect.drawRect(this.container.width, 0, 200, 200);
       this.container.addChild(rect);
 
       this.containerWidth = this.container.width;
       this.containerHeight = this.container.height;
-
-      const { width, height, x, y } = contain(this.width, this.height, this.containerWidth, this.containerHeight);
-      this.container.width = width;
-      this.container.height = height;
-      this.container.position.set(x, y);
+      this.resizeContainer();
     }
 
     PIXI.Loader.shared
@@ -145,14 +133,72 @@ class Animation {
       ])
       .load(setup);
 
+    this.styleTitles();
     this.playButton.addEventListener('click', this.play.bind(this));
     this.stopButton.addEventListener('click', this.stop.bind(this));
+  }
+
+  coordinateTitles() {
+    this.hovers.forEach( (hover, index) => {
+      const title = this.titles[index];
+      const options = this.titlesOptions[index];
+      const xPosition = hover.getGlobalPosition().x + (this.container.width * options.x / this.containerWidth);
+      const yPosition = hover.getGlobalPosition().y + (this.container.height * options.y / this.containerHeight);
+
+      if(title.classList.contains('js-rightTitle')) {
+        title.style.right = `${xPosition  }px`;
+      } else {
+        title.style.left = `${xPosition  }px`;
+      }
+
+      title.style.top = `${yPosition  }px`;
+    })
+  }
+
+  styleTitles() {
+    this.titles.forEach( (title, index) => {
+      const text = title.querySelector('.js-text')
+      const options = this.titlesOptions[index];
+
+      text.style.backgroundColor = options.background;
+      text.style.color = options.color;
+    })
+  }
+
+  resizeContainer() {
+    const { width, height, x, y } = contain(this.width, this.height, this.containerWidth, this.containerHeight);
+    this.container.width = width;
+    this.container.height = height;
+    this.container.position.set(x, y);
+    
+    this.coordinateTitles();
+  }
+
+  addHoverListaners() {
+    /* eslint no-param-reassign: ["error", { "props": false }] */
+    this.hovers.forEach ( (hover) => {
+      hover.anchor.set(0.5);
+      hover.scale.set(0.52)
+      hover.interactive = true;
+      hover.buttonMode = true;
+
+      hover.on('pointerover', this.mouseOverRoom.bind(this, hover));
+      hover.on('pointerout', this.mouseOffRoom.bind(this, hover));
+    });
+  }
+
+  coordinateRooms() {
+    this.container.children.forEach ( (room, index) => {
+      room.scale.set(0.6);
+      room.position.set(this.roomsOptions[index].x, this.roomsOptions[index].y);
+    });
   }
 
   play() {
     this.stopAnimation = false;
 
     this.container.children.forEach ( (room, index) => {
+      if(!this.roomsOptions[index]) return;
       this.flyRoom(room, this.roomsOptions[index].y, this.roomsOptions[index].yPath, this.roomsOptions[index].speed, index);
     });
   }
@@ -175,20 +221,13 @@ class Animation {
   }
 
   mouseOverRoom(hover) {
-    if (hover.scale.x >= 0.6) return;
-    requestAnimationFrame( this.mouseOverRoom.bind(this, hover) );
-
-    hover.scale.set(hover.scale.x + 0.01)
-    console.log(hover.scale.x)
+    gsap.to(hover.scale, {x: 0.6, y: 0.6, duration: 0.3})
+    console.log(this.container.scale.x)
   }
 
   mouseOffRoom(hover) {
-
-    if (hover.scale.x <= 0.522) return;
-    requestAnimationFrame( this.mouseOffRoom.bind(this, hover) );
-
-    hover.scale.set(hover.scale.x - 0.01)
-    console.log(hover.scale.x)
+    gsap.to(hover.scale, {x: 0.52, y: 0.52, duration: 0.3})
+    console.log(this.container.scale.x)
   }
 
   resize() {
@@ -196,16 +235,14 @@ class Animation {
     this.height = this.animationContainer.clientHeight;
 
     this.canvas.renderer.resize(this.width, this.height);
-    
-    const { width, height, x, y } = contain(this.width, this.height, this.containerWidth, this.containerHeight);
-    this.container.width = width;
-    this.container.height = height;
-    this.container.position.set(x, y);
+    this.resizeContainer();
   }
 };
 
-const playButton = document.getElementById('playAnimation');
-const stopButton = document.getElementById('stopAnimation');
-const animationControl = new Animation({playButton, stopButton});
+const titlesContainer = document.querySelector('.js-titlesContainer')
+const container = document.querySelector('.js-container');
+const playButton = document.querySelector('.js-play');
+const stopButton = document.querySelector('.js-stop');
+const animationControl = new Animation({container, playButton, stopButton, titlesContainer});
 
 sayHello();
